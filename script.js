a4c7fbe2f4012481ae2a390eaa491973112dd504
DonatellaC <donatella.carta@mailbox.org>
2022-02-10 23:14:35 +0100

    fixed functionality

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ const numberToGuess = document.querySelector(".number");
 const guessedNumber = document.querySelector(".guess");
 const message = document.querySelector(".message");
 const againButton = document.querySelector(".again");
+const score = document.querySelector(".score");
 
 //Number to guess
 const randomNumber = Math.floor(Math.random() * 20) + 1;
 
 //Check button
 const handleCheck = () => {
-  numberToGuess.textContent = randomNumber;
-  if (guessedNumber.value === randomNumber) {
+  const guess = Number(guessedNumber.value);
+  const numberToGuessValue = Number(numberToGuess.value);
+  console.log(guess, typeof guess);
+  numberToGuess.textContent = numberToGuessValue;
+  if (guess === numberToGuessValue) {
     message.textContent = "Correct!";
-  } else {
+  } else if (guess !== numberToGuessValue) {
     message.textContent = "Wrong guess! Try again!";
   }
 };
@@ -20,8 +24,11 @@ checkButton.addEventListener("click", handleCheck);
 
 // Again button
 const handleAgain = () => {
+  const randomNumber = Math.floor(Math.random() * 20) + 1;
   numberToGuess.textContent = "?";
   message.textContent = "Start guessing...";
   guessedNumber.value = "";
+  console.log((numberToGuess.value = randomNumber));
+  numberToGuess.value = randomNumber;
 };
 againButton.addEventListener("click", handleAgain);
