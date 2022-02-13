bcebc744e9b09735a5e5d12339fe8cff3a00447b
DonatellaC <donatella.carta@mailbox.org>
2022-02-13 19:20:23 +0100

    added functionality number too high/low and refactor

--- script.js
+++ script.js
@@ -4,33 +4,57 @@ const guessedNumber = document.querySelector(".guess");
 const message = document.querySelector(".message");
 const againButton = document.querySelector(".again");
 const score = document.querySelector(".score");
+const highscore = document.querySelector(".highscore");
 
-//Number to guess
-const randomNumber = Math.floor(Math.random() * 20) + 1;
+let randomNumber = Math.floor(Math.random() * 20) + 1;
+let scoreResult = 20;
+let highscoreResult = 0;
 
-let scoreResult = 0;
+const initialValues = () => {
+  score.textContent = 20;
+  highscoreResult.textContent = 0;
+  guessedNumber.value = "";
+  numberToGuess.textContent = "?";
+  message.textContent = "Start guessing...";
+  numberToGuess.value = randomNumber;
+};
+initialValues();
 
-//Check button
+//Player wins
 const handleCheck = () => {
   const guess = Number(guessedNumber.value);
   const numberToGuessValue = Number(numberToGuess.value);
-  numberToGuess.textContent = numberToGuessValue;
-  if (guess === numberToGuessValue) {
+  if (!guess || guess < 0 || guess > 20) {
+    numberToGuess.textContent = "?";
+    message.textContent = "⛔️ Insert a valid number!";
+    //Correct number
+  } else if (guess === randomNumber) {
     message.textContent = "Correct!";
     scoreResult++;
     score.textContent = scoreResult;
-  } else if (guess !== numberToGuessValue) {
-    message.textContent = "Wrong guess! Try again!";
+    document.body.style.backgroundColor = "#60b347";
+    if (scoreResult > highscoreResult) {
+      highscoreResult = scoreResult;
+      highscore.textContent = highscoreResult;
+    }
+    numberToGuess.textContent = Number(numberToGuessValue);
+    //Too high number
+  } else if (guess > randomNumber) {
+    message.textContent = "📈 The number is too high!";
+    scoreResult--;
+    //Too low number
+  } else if (guess < randomNumber) {
+    message.textContent = "📉 The number is too low!";
+    scoreResult--;
   }
 };
 checkButton.addEventListener("click", handleCheck);
 
-// Again button
+// Play again
 const handleAgain = () => {
-  const randomNumber = Math.floor(Math.random() * 20) + 1;
-  numberToGuess.textContent = "?";
-  message.textContent = "Start guessing...";
-  guessedNumber.value = "";
+  let randomNumber = Math.floor(Math.random() * 20) + 1;
   numberToGuess.value = randomNumber;
+  initialValues();
+  document.body.style.backgroundColor = "#222";
 };
 againButton.addEventListener("click", handleAgain);
